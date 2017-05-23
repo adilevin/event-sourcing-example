ddb74addf8abd2a6e98a15b0366d54a73230aac6
Adi Levin <alevin@aligntech.com>
2017-05-23 13:38:28 +0300

    Extract Projector base class

--- messages_projector.py
+++ messages_projector.py
@@ -1,49 +1,26 @@
 import messaging_events
+from projector import Projector
 
 
-class MessagesProjector(object):
+class MessagesProjector(Projector):
 
     def __init__(self, event_store, messages_store):
-        self.event_store = event_store
-        self.messages_store = messages_store
-        self.updaters = {
-            messaging_events.MESSAGE_READ_BY_USER: self._on_msg_read,
-            messaging_events.MESSAGE_SENT_TO_USER: self._on_msg_sent
-        }
-
-    def run_infinite_refresh_loop(self, seconds_to_sleep, max_num_events_to_process):
-        import time, sys
-        while True:
-            if self.refresh(max_num_events_to_process):
-                character = 'x'
-            else:
-                character = '.'
-            sys.stdout.write(character)
-            sys.stdout.flush()
-            time.sleep(seconds_to_sleep)
-
-    def refresh(self, max_num_events_to_process):
-        cur_seq_num = self.messages_store.get_cur_seq_num()
-        any_events_found = False
-        events = self.event_store.get_events(
-            limit=max_num_events_to_process, from_seq_num=cur_seq_num+1)
-        for event in events:
-            self.updaters[event["event_type"]].__call__(event)
-            any_events_found = True
-        return any_events_found
-
-    def _update_by_event(self, event):
-        event_type = event["event_type"]
-        self.updaters[event_type].__call__(event)
+        super(self.__class__, self).__init__(
+            event_store=event_store,
+            projection_store=messages_store,
+            updaters={
+                messaging_events.MESSAGE_READ_BY_USER: self._on_msg_read,
+                messaging_events.MESSAGE_SENT_TO_USER: self._on_msg_sent
+            })
 
     def _on_msg_read(self, event):
-        self.messages_store.mark_msg_as_read(
+        self.projection_store.mark_msg_as_read(
             user_id=event["aggregate_id"],
             message_id=event["message_id"],
             new_seq_num=event["seq_num"])
 
     def _on_msg_sent(self, event):
-        self.messages_store.add_msg_for_user(
+        self.projection_store.add_msg_for_user(
             user_id=event["aggregate_id"],
             message_id=event["message_id"],
             new_seq_num=event["seq_num"])
---
+++ projector.py
@@ -0,0 +1,32 @@
+
+class Projector(object):
+
+    def __init__(self, event_store, projection_store, updaters):
+        self.event_store = event_store
+        self.projection_store = projection_store
+        self.updaters = updaters
+
+    def run_infinite_refresh_loop(self, seconds_to_sleep, max_num_events_to_process):
+        import time, sys
+        while True:
+            if self.refresh(max_num_events_to_process):
+                character = 'x'
+            else:
+                character = '.'
+            sys.stdout.write(character)
+            sys.stdout.flush()
+            time.sleep(seconds_to_sleep)
+
+    def refresh(self, max_num_events_to_process):
+        cur_seq_num = self.projection_store.get_cur_seq_num()
+        any_events_found = False
+        events = self.event_store.get_events(
+            limit=max_num_events_to_process, from_seq_num=cur_seq_num+1)
+        for event in events:
+            self.updaters[event["event_type"]].__call__(event)
+            any_events_found = True
+        return any_events_found
+
+    def _update_by_event(self, event):
+        event_type = event["event_type"]
+        self.updaters[event_type].__call__(event)
