7be713f1c1c62525c1ddd178ce2b9dcf8964e537
Adi Levin <alevin@aligntech.com>
2017-05-25 13:35:59 +0300

    Improve msg demo

---
+++ msg_query_handler.py
@@ -0,0 +1,13 @@
+
+class MsgQueryHandler(object):
+
+    def __init__(self, msg_projection):
+        # The query handler gets the projection as a dependency, especially because
+        # it can have a number of projections as input.
+        self.msg_projection = msg_projection
+
+    def get_msgs(self, user_id):
+        return self.msg_projection.get_msgs(user_id)
+
+    def get_num_unread(self, user_id):
+        return self.msg_projection.get_num_read(user_id)
